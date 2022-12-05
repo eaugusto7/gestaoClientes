f76a580b41d90b96a1ccba5c3a59680ba8944c08
Eduardo Augusto <edborges96@gmail.com>
2022-12-05 11:39:01 -0300

    Criação de Models Cliente

---
+++ models/cliente.go
@@ -0,0 +1,14 @@
+package models
+
+type Cliente struct {
+	Id             int
+	Nome           string
+	Cpf            string
+	Rg             string
+	Email          string
+	Telefone       string
+	Celular        string
+	DataNascimento string
+	Sexo           string
+	Profissao      string
+}
