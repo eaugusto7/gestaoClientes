26d3fcf7a37ae76f8707b6e58a70c02b93e72aa8
eaugusto7 <edborges96@gmail.com>
2022-12-19 16:25:27 -0300

    Testes modelo Quadro Horários

--- quadrohorarios_test.go
+++ quadrohorarios_test.go
@@ -1,1 +1,146 @@
 package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/eaugusto7/gestaoClientes/controllers"
+	database "github.com/eaugusto7/gestaoClientes/database"
+	"github.com/eaugusto7/gestaoClientes/models"
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+)
+
+var ID_QuadroHorarios int
+
+func CriaHorarioMock() {
+	horario := models.Quadroshorarios{
+		Domingo: []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Segunda: []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Terca:   []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Quarta:  []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Quinta:  []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Sexta:   []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Sabado:  []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	database.Database.Create(&horario)
+	ID_QuadroHorarios = int(horario.Id)
+}
+
+func CriaHorarioModel() models.Quadroshorarios {
+	horario := models.Quadroshorarios{
+		Domingo: []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Segunda: []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Terca:   []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Quarta:  []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Quinta:  []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Sexta:   []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		Sabado:  []int64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	return horario
+}
+
+type JsonEdicaoHorario struct {
+	Domingo pq.Int64Array `gorm:"type:integer[]"`
+}
+
+func DeletaHorarioMock() {
+	var horario models.Quadroshorarios
+	database.Database.Delete(&horario, ID_QuadroHorarios)
+}
+
+func TestGetAllHorarios(t *testing.T) {
+	database.ConectaBanco()
+	CriaHorarioMock()
+	defer DeletaHorarioMock()
+
+	r := SetupDasRotasDeTeste()
+	r.GET("/api/v1/horarios", controllers.GetAllHorarios)
+	req, _ := http.NewRequest("GET", "/api/v1/horarios", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestGetHorarioById(t *testing.T) {
+	database.ConectaBanco()
+	CriaHorarioMock()
+	defer DeletaHorarioMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/api/v1/horarios/:id", controllers.GetHorarioById)
+	req, _ := http.NewRequest("GET", "/api/v1/horarios/"+strconv.Itoa(ID_QuadroHorarios), nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+
+	var horarioMock models.Quadroshorarios
+	json.Unmarshal(resposta.Body.Bytes(), &horarioMock)
+
+}
+
+func TestInsertHorario(t *testing.T) {
+	database.ConectaBanco()
+
+	r := SetupDasRotasDeTeste()
+	r.POST("/api/v1/horarios", controllers.InsertQuadroHorario)
+
+	horarioModelo := CriaHorarioModel()
+
+	jsonValue, _ := json.Marshal(horarioModelo)
+	req, _ := http.NewRequest("POST", "/api/v1/horarios", bytes.NewBuffer(jsonValue))
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+
+	//Converte Response para Map
+	bodyBytes, err := io.ReadAll(resposta.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+
+	mapResponse := map[string]interface{}{}
+	if err := json.Unmarshal([]byte(bodyString), &mapResponse); err != nil {
+		panic(err)
+	}
+
+	//Deleta ClienteMock gerado
+	database.Database.Delete(&horarioModelo, mapResponse["Id"])
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestUpdateHorario(t *testing.T) {
+	database.ConectaBanco()
+	CriaHorarioMock()
+	defer DeletaHorarioMock()
+	r := SetupDasRotasDeTeste()
+
+	r.PATCH("/api/v1/horarios/:id", controllers.UpdateHorarios)
+	pathEdit := "/api/v1/horarios/" + strconv.Itoa(ID_QuadroHorarios)
+
+	var modeloJson JsonEdicaoHorario
+	modeloJson.Domingo = []int64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	valorJson, _ := json.Marshal(modeloJson)
+
+	req, _ := http.NewRequest("PATCH", pathEdit, bytes.NewBuffer(valorJson))
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+
+	var horarioMockAtualizado models.Quadroshorarios
+	json.Unmarshal(resposta.Body.Bytes(), &horarioMockAtualizado)
+}
+
+func TestDeleteHorario(t *testing.T) {
+	database.ConectaBanco()
+	CriaHorarioMock()
+	r := SetupDasRotasDeTeste()
+	r.DELETE("/api/v1/horarios/:id", controllers.DeleteHorario)
+	req, _ := http.NewRequest("DELETE", "/api/v1/horarios/"+strconv.Itoa(ID_QuadroHorarios), nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
